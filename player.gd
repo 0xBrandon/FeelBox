331c7bcfa6858179ddb8e15e39fae1122a93bcfd
Brandon Hill <122886834+0xBrandon@users.noreply.github.com>
2026-08-27 12:59:04 -0500

    V2: Variable Jump Height

--- player.gd
+++ player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 const MAX_SPEED    := 300.0
-const ACCEL        := 2200.0   # how fast you reach top speed on the ground
-const FRICTION     := 600.0   # how fast you stop on the ground
-const AIR_ACCEL    := 1500.0   # less control in the air...
-const AIR_FRICTION := 320.0    # ...and you barely slow down up there
+const ACCEL        := 2200.0
+const FRICTION     := 2600.0
+const AIR_ACCEL    := 1500.0
+const AIR_FRICTION := 320.0
 
 const JUMP_VELOCITY := -520.0
+const JUMP_CUT      := 0.40    # <- the new one
 const GRAVITY       := 1400.0
 
 
@@ -24,5 +25,9 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	# Let go while still rising? Kill most of the upward speed.
+	if Input.is_action_just_released("jump") and velocity.y < 0.0:
+		velocity.y *= JUMP_CUT
 
 	move_and_slide()
